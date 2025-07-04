3266737b00379df75528f355791616a23f6e289a
Dzelle Faith Tan <dzellefaith.tan@GXCHLAP00387.local>
2025-07-04 11:50:39 +0800

    feat: Implement Service and ServiceFactory classes using Factory pattern

--- src/main/java/org/example/model/Service.java
+++ src/main/java/org/example/model/Service.java
@@ -1,6 +1,30 @@
 package org.example.model;
-// Dzelle will implement this
+
 public class Service {
-    // To be implemented: private fields for serviceName and cost
-    // To be implemented: constructor and getters
+    private String serviceName;
+    private float cost;
+
+    /**
+     * Constructs a new Service with a specified name and cost.
+     * @param serviceName The name of the service (e.g., "X-Ray").
+     * @param cost The cost of the service.
+     */
+    public Service(String serviceName, float cost) {
+        this.serviceName = serviceName;
+        this.cost = cost;
+    }
+
+    /**
+     * @return The name of the service.
+     */
+    public String getServiceName() {
+        return serviceName;
+    }
+
+    /**
+     * @return The cost of the service.
+     */
+    public float getCost() {
+        return cost;
+    }
 }
